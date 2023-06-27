cbe87cfdb69cbdb2c0794dd0cb119617cd445d46
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-27 08:07:01 +0300

    running n updating the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_045351) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_050524) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
