0d855e0ec854954aceec43bb055778142e5dbe1d
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-27 08:15:24 +0300

    adding validations to the order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  
 end
--- db/migrate/20230627050953_create_orders.rb
+++ db/migrate/20230627050953_create_orders.rb
@@ -1,7 +1,7 @@
-class CreateOrders < ActiveRecord::Migration[7.0]
+class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true, null: false
       t.decimal :total_price
       t.string :status
       t.text :shipping_address
