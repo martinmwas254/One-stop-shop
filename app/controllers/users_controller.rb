22ca5fdd9598648a8a97d7df236421d47222d0f1
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-28 11:59:57 +0300

    adding actions  to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    # fetching all users
+    def index
+        users = User.all
+        render json: users
+    end
+
+    # creating a new user 
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+
 end
